f22147138609f5beff6f90c487b6683148af39ea
Astha <asthabhartiab84@gmail.com>
2023-08-25 11:36:39 +0530

    right one

--- labassign.py
+++ labassign.py
@@ -1,62 +1,50 @@
-class Process:
-    def __init__(self, pid, process_name, start_time, priority):
-        self.pid = pid
-        self.process_name = process_name
-        self.start_time = start_time
-        self.priority = priority
-
 class FlightTable:
-    def __init__(self):
-        self.processes = []
+    def _init_(self):
+        self.data = []
 
-    def add_process(self, process):
-        self.processes.append(process)
+    def add_entry(self, p_id, process, start_time, priority):
+        self.data.append({'P_ID': p_id, 'Process': process, 'Start Time': start_time, 'Priority': priority})
 
-    def sort_by_pid(self):
-        self.processes.sort(key=lambda x: x.pid)
+    def bubble_sort(self, key):
+        n = len(self.data)
+        for i in range(n - 1):
+            for j in range(0, n - i - 1):
+                if self.data[j][key] > self.data[j + 1][key]:
+                    self.data[j], self.data[j + 1] = self.data[j + 1], self.data[j]
 
-    def sort_by_start_time(self):
-        self.processes.sort(key=lambda x: x.start_time)
+    def print_table(self):
+        print("{:<5} {:<10} {:<15} {:<10}".format("P_ID", "Process", "Start Time", "Priority"))
+        print("="*40)
+        for entry in self.data:
+            print("{:<5} {:<10} {:<15} {:<10}".format(entry['P_ID'], entry['Process'], entry['Start Time'], entry['Priority']))
 
-    def sort_by_priority(self):
-        priority_order = {'Low': 0, 'MID': 1, 'High': 2}
-        self.processes.sort(key=lambda x: priority_order[x.priority])
+flight_table = FlightTable()
+flight_table.add_entry("P1", "VSCode", 100, "MID")
+flight_table.add_entry("P23", "Eclipse", 234, "MID")
+flight_table.add_entry("P93", "Chrome", 189, "High")
+flight_table.add_entry("P42", "JDK", 9, "High")
+flight_table.add_entry("P9", "CMD", 7, "High")
+flight_table.add_entry("P87", "NotePad", 23, "Low")
 
-    def display(self):
-        print("{:<5} {:<10} {:<15} {:<8}".format("P_ID", "Process", "Start Time", "Priority"))
-        print("=" * 40)
-        for process in self.processes:
-            print("{:<5} {:<10} {:<15} {:<8}".format(process.pid, process.process_name, process.start_time, process.priority))
+while True:
+    print("\nSorting Options:")
+    print("1. Sort by P_ID")
+    print("2. Sort by Start Time")
+    print("3. Sort by Priority")
+    print("4. Quit")
 
-def main():
-    flight_table = FlightTable()
+    choice = int(input("Enter your choice: "))
 
-    flight_table.add_process(Process("P1", "VSCode", 100, "MID"))
-    flight_table.add_process(Process("P23", "Eclipse", 234, "MID"))
-    flight_table.add_process(Process("P93", "Chrome", 189, "High"))
-    flight_table.add_process(Process("P42", "JDK", 9, "High"))
-    flight_table.add_process(Process("P9", "CMD", 7, "High"))
-    flight_table.add_process(Process("P87", "NotePad", 23, "Low"))
+    if choice == 1:
+        flight_table.bubble_sort('P_ID')
+    elif choice == 2:
+        flight_table.bubble_sort('Start Time')
+    elif choice == 3:
+        flight_table.bubble_sort('Priority')
+    elif choice == 4:
+        break
+    else:
+        print("Invalid choice. Please enter a valid option.")
 
-    while True:
-        print("\nChoose sorting parameter:")
-        print("1. Sort by P_ID")
-        print("2. Sort by Start Time")
-        print("3. Sort by Priority")
-        choice = input("Enter your choice (1/2/3): ")
-
-        if choice == '1':
-            flight_table.sort_by_pid()
-        elif choice == '2':
-            flight_table.sort_by_start_time()
-        elif choice == '3':
-            flight_table.sort_by_priority()
-        else:
-            print("Invalid choice. Exiting...")
-            break
-
-        flight_table.display()
-
-if __name__ == "__main__":
-    main()
+    flight_table.print_table()
 
